95796decd5f99ec313f29d8fca0ba29760e9caf5
kikinacademy <kikinacademy@gmail.com>
2022-11-02 19:36:46 -0600

    SRCS files

---
+++ src/data structures/list/list.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "doctor.h"
+#include "patient.h"
+
+class list
+{
+public:
+	list();
+	~list();
+
+	void insert(const doctor&);
+	void show_full();
+	bool is_empty() const;
+
+private:
+	struct node
+	{
+		doctor data;
+		node* next{};
+	};
+
+	node *head_, *tail_, *temp_;
+};
