04ec5ed6b1d8af2297e395a703bd4f5544ad0d2b
kikinacademy <kikinacademy@gmail.com>
2022-11-15 22:25:21 -0600

    EntregaFinal

---
+++ src/doctor.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class doctor
+{
+public:
+	doctor();
+	doctor(string, int, int, string);
+
+	string name{};
+	int age{};
+	int dr_id{};
+	string specialty{};
+
+	void details() const;
+};
